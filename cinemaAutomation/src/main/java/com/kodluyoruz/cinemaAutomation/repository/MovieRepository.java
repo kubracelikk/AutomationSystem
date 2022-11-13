d7e7dc265d18c2c68d10ac286f93fa3e5a57f3df
Kubra Celik <kubracelikce@gmail.com>
2022-11-13 19:46:32 +0300

    updated

--- cinemaAutomation/src/main/java/com/kodluyoruz/cinemaAutomation/repository/MovieRepository.java
+++ cinemaAutomation/src/main/java/com/kodluyoruz/cinemaAutomation/repository/MovieRepository.java
@@ -4,10 +4,9 @@ package com.kodluyoruz.cinemaAutomation.repository;
 	
     import com.kodluyoruz.cinemaAutomation.entities.Movie;
 	import org.springframework.data.jpa.repository.JpaRepository;
-	import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-	public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+	public interface MovieRepository extends JpaRepository<Movie, Long> {
 	}
 
 
